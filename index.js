ed4c6280400c8a1591141144c7e7b632a68648ac
mwyd <58394803+mwyd@users.noreply.github.com>
2020-09-20 22:53:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+window.onload = () => {
+    chrome.runtime.sendMessage({action: 'get_style', params: {}}, res => {
+        const {data} = res
+        if(data.success) {
+            console.log('[SP-BOT] style load success')
+            
+            let styleElement = document.createElement('style')
+            styleElement.type = 'text/css'
+            styleElement.rel = 'stylesheet'
+            styleElement.innerHTML = data.style
+            document.querySelector('head').appendChild(styleElement)
 
+            chrome.runtime.sendMessage({action: 'get_script', params: {}}, res => {
+                const {data} = res
+                if(data.success) {
+                    console.log('[SP-BOT] script load success')
+                    eval(data.script)
+                }
+                else console.log('[SP-BOT] script load fail')
+            })
+        }
+        else console.log(['[SP-BOT] style load fail'])
+    })
+}
