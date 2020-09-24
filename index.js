6cb4f4f161c2d6b2fb1736ad6bb8876122795837
mwyd <58394803+mwyd@users.noreply.github.com>
2020-09-24 16:48:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 window.onload = () => {
     chrome.runtime.sendMessage({action: 'get_style', params: {}}, res => {
-        const {data} = res
+        const {data} = res;
         if(data.success) {
-            console.log('[' + new Date().toLocaleTimeString() + '] [SP-BOT] style load success')
+            console.log('[' + new Date().toLocaleTimeString() + '] [SP-BOT] style load success');
             
-            let styleElement = document.createElement('style')
-            styleElement.type = 'text/css'
-            styleElement.rel = 'stylesheet'
-            styleElement.innerHTML = data.style
-            document.querySelector('head').appendChild(styleElement)
+            let styleElement = document.createElement('style');
+            styleElement.type = 'text/css';
+            styleElement.rel = 'stylesheet';
+            styleElement.innerHTML = data.style;
+            document.querySelector('head').appendChild(styleElement);
 
             chrome.runtime.sendMessage({action: 'get_script', params: {}}, res => {
-                const {data} = res
+                const {data} = res;
                 if(data.success) {
-                    console.log('[' + new Date().toLocaleTimeString() + '] [SP-BOT] script load success')
-                    eval(data.script)
+                    console.log('[' + new Date().toLocaleTimeString() + '] [SP-BOT] script load success');
+                    eval(data.script);
                 }
-                else console.log('[' + new Date().toLocaleTimeString() + '] [SP-BOT] script load fail')
-            })
+                else console.log('[' + new Date().toLocaleTimeString() + '] [SP-BOT] script load fail');
+            });
         }
-        else console.log('[' + new Date().toLocaleTimeString() + '] [SP-BOT] style load fail')
-    })
+        else console.log('[' + new Date().toLocaleTimeString() + '] [SP-BOT] style load fail');
+    });
 }
